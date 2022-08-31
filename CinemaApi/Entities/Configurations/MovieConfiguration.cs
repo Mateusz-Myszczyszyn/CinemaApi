295bea62e312fc3cc5eb9d7a66238892f096509a
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-08-31 22:32:16 +0200

    Adding DbContext Configuration

---
+++ CinemaApi/Entities/Configurations/MovieConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Entities.Configurations
+{
+    public class MovieConfiguration : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            builder.Property(m => m.Title).IsRequired();
+            builder.Property(m => m.Director).IsRequired();
+            builder.Property(m => m.Description).HasMaxLength(100);
+
+           
+            
+        }
+    }
+}
