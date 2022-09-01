375f0375b3fdcfd772944a384a2875f791fa8a2f
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-09-01 13:42:15 +0200

    Configuration

---
+++ CinemaApi/Entities/Configurations/MoviePerformingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Entities.Configurations
+{
+    public class MoviePerformingConfiguration : IEntityTypeConfiguration<MoviePerforming>
+    {
+        public void Configure(EntityTypeBuilder<MoviePerforming> builder)
+        {
+            builder.Property(x => x.MovieShowDates).HasColumnName("Movie_Shows");
+        }
+    }
+}
--- CinemaApi/Entities/Role.cs
+++ CinemaApi/Entities/Role.cs
@@ -10,8 +10,6 @@ namespace CinemaApi.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<User> Users { get; set; }
-
-
+        
     }
 }
