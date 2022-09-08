436d5489bfe5a7987ef8bb13820fafd46dbf16bf
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-09-08 22:25:18 +0200

    Entity rework migration

---
+++ CinemaApi/Migrations/20220908202103_Rework_Role_Entity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaApi.Migrations
+{
+    public partial class Rework_Role_Entity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 4, "Worker" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
