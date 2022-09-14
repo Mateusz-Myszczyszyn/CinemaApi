904d66529293f470856698e341dce49931884d46
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-09-14 21:58:52 +0200

    Adding NuGet packages and applying middleware on program.cs

--- CinemaApi/Program.cs
+++ CinemaApi/Program.cs
@@ -1,5 +1,6 @@
 using CinemaApi;
 using CinemaApi.Entities;
+using CinemaApi.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 builder.Services.AddDbContext<CinemaDbContext>(
     options => options
@@ -34,7 +36,8 @@ if (pendingMigration.Any())
     dbContext.Database.Migrate();
 }
 
-DataSeeder.Seed(dbContext);
+//DataSeeder.Seed(dbContext);
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
