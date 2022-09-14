73522699a3a9e9286fbbad5f14dd65ec4a3ce8ee
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-09-14 21:58:10 +0200

    Adding middleware for handling errors

---
+++ CinemaApi/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using CinemaApi.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch(BadRequestException badRequest)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequest.Message);
+            }
+            catch(NotFoundException notFound)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFound.Message);
+            }
+            catch(ForbiddenAccessException forbid)
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync(forbid.Message);
+            }
+            catch(Exception e)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync(e.Message);
+            }
+        }
+    }
+}
