e6139c76175c92da6e66a46f318b1ba4f8100f83
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-09-16 22:40:15 +0200

    Cinema Controller not finished

---
+++ CinemaApi/Controllers/CinemaController.cs
@@ -0,0 +1,40 @@
+﻿using CinemaApi.Entities;
+using CinemaApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Controllers
+{
+    [Route("api/cinema")]
+    [ApiController]
+    public class CinemaController
+    {
+        private readonly ICinemaService _service;
+
+        public CinemaController(ICinemaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IResult GetAll()
+        {
+            var cinemas =_service.GetAll();
+
+            return Results.Ok(cinemas);
+            
+        }
+
+        [HttpGet("/{id}")]
+        public IResult GetById([FromRoute]int id)
+        {
+            var cinema = _service.GetById(id);
+
+            return Results.Ok(cinema);
+        }
+    }
+}
