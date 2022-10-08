637a266ed80602d5c8abea9df78c4ba5ce231c27
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-10-08 17:46:41 +0200

    Applying new column to entity ScreenPlay

--- CinemaApi/Migrations/20221008145514_scrplay_entity_columnadd.Designer.cs
+++ CinemaApi/Migrations/20221008145514_scrplay_entity_columnadd.Designer.cs
@@ -4,6 +4,7 @@ using CinemaApi.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CinemaApi.Migrations
 {
     [DbContext(typeof(CinemaDbContext))]
-    partial class CinemaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221008145514_scrplay_entity_columnadd")]
+    partial class scrplay_entity_columnadd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CinemaApi/Migrations/20221008145514_scrplay_entity_columnadd.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaApi.Migrations
+{
+    public partial class scrplay_entity_columnadd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DigitalView",
+                table: "ScreenPlays",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "2D");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DigitalView",
+                table: "ScreenPlays");
+        }
+    }
+}
