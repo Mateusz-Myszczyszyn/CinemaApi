51d565796dc51b1eaa26a5c426d0b8348cc53074
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-10-12 22:51:36 +0200

    Adding UserContextService to access user information.

---
+++ CinemaApi/Services/IUserContextService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace CinemaApi.Services
+{
+    public interface IUserContextService
+    {
+        int? GetUserId { get; }
+        ClaimsPrincipal User { get; }
+    }
+}
---
+++ CinemaApi/Services/UserContextService.cs
@@ -0,0 +1,26 @@
+﻿using CinemaApi.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Services
+{
+    public class UserContextService : IUserContextService
+    {
+        private readonly HttpContextAccessor _httpContext;
+
+        public UserContextService(HttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public ClaimsPrincipal User => _httpContext.HttpContext.User;
+
+        public int? GetUserId =>
+            User is null ? null : (int?)int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
+    }
+}
