c40cc20b14be1f2b250513c2bbe9b27d86bb62f5
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-10-12 22:46:31 +0200

    Adding dto to update seatReservation

---
+++ CinemaApi/Dtos/CreateDtos/UpdateSeatReservationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Dtos.CreateDtos
+{
+    public class UpdateSeatReservationDto
+    {
+        public bool IsReserved { get; set; }
+        public bool Active { get; set; }
+        public int HallSeatId { get; set; }
+        public int ScreenPlayId { get; set; }
+    }
+}
