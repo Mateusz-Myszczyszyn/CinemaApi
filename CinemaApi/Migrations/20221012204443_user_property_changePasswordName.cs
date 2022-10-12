56f238a44e713f2286daf573bcdcf66629c50e5d
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-10-12 22:45:12 +0200

    Changing Users property

---
+++ CinemaApi/Migrations/20221012204443_user_property_changePasswordName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaApi.Migrations
+{
+    public partial class user_property_changePasswordName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "Password");
+        }
+    }
+}
