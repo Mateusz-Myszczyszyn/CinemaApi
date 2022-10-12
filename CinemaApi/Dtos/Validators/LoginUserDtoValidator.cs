4b7499e0683aebfb3b58c606447dd267f0ba65c5
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-10-12 22:49:37 +0200

    Adding creating user and login user dto with registration validator and login validator

---
+++ CinemaApi/Dtos/Validators/LoginUserDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using CinemaApi.Dtos.EntitiesDtos;
+using CinemaApi.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Dtos.Validators
+{
+    public class LoginUserDtoValidator : AbstractValidator<LoginUserDto>
+    {
+        public LoginUserDtoValidator(CinemaDbContext dbContext)
+        {
+            RuleFor(c => c.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(c => c.Password)
+                .NotEmpty()
+                .MinimumLength(10);
+        }
+    }
+}
---
+++ CinemaApi/Dtos/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,41 @@
+﻿using CinemaApi.Dtos.CreateDtos;
+using CinemaApi.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Dtos.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator(CinemaDbContext dbContext)
+        {
+            RuleFor(c => c.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(c => c.Password)
+                .NotEmpty()
+                .MinimumLength(10);
+
+            RuleFor(c => c.ConfirmPassword)
+                .NotEmpty()
+                .Equal(c => c.Password);
+
+            RuleFor(c => c.Email)
+                .Custom((value, context) =>
+                {
+                    var checkIfUsed = dbContext.Users.Any(c => c.Email == value);
+                    if (checkIfUsed)
+                    {
+                        context.AddFailure("Email", "That email is taken");
+                    }
+                });
+
+            
+        }
+    }
+}
