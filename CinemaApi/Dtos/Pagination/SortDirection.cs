1babd5862778d8055c62ac4d6e61d676ddc107d9
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-11-05 22:52:59 +0100

    Adding pagination

---
+++ CinemaApi/Dtos/Pagination/SortDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Dtos.Pagination
+{
+    public enum SortDirection
+    {
+        ASC,
+        DESC
+    }
+}
