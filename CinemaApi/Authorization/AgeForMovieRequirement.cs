14f56d3a6ec4a417f44c391b839affeb57006620
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-11-05 22:52:26 +0100

    Adding authorization requirement

---
+++ CinemaApi/Authorization/AgeForMovieRequirement.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Authorization
+{
+    public class AgeForMovieRequirement : IAuthorizationRequirement
+    {
+        public int Age { get;}
+
+        public AgeForMovieRequirement(int age)
+        {
+            Age = age;
+        }
+    }
+}
---
+++ CinemaApi/Authorization/AgeForMovieRequirementHandler.cs
@@ -0,0 +1,39 @@
+﻿using CinemaApi.Entities;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Authorization
+{
+    public class AgeForMovieRequirementHandler : AuthorizationHandler<AgeForMovieRequirement>
+    {
+        private readonly ILogger<AgeForMovieRequirement> _logger;
+
+        public AgeForMovieRequirementHandler(ILogger<AgeForMovieRequirement> logger)
+        {
+            _logger = logger;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AgeForMovieRequirement requirement)
+        {
+           
+            var UserDate = DateTime.Parse(context.User.FindFirst(c => c.Type == "DateOfBirth").Value);
+            var UserEmail = context.User.FindFirst(c => c.Type == ClaimTypes.Email).Value;
+
+            if(UserDate.AddYears(requirement.Age) <= DateTime.Today)
+            {
+                _logger.LogInformation($"User with email{UserEmail} and DateOfBirth {UserDate} done action");
+                context.Succeed(requirement);
+            }
+            else
+            {
+                _logger.LogInformation("Authorization failed");
+            }
+            return Task.CompletedTask;
+
+        }
+    }
+}
