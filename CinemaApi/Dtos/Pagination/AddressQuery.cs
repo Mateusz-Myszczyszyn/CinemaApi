028ecbc674eec1ac752ec1588879312ddf1a7889
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-11-15 22:08:39 +0100

    Pagination add

---
+++ CinemaApi/Dtos/Pagination/AddressQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Dtos.Pagination
+{
+    public class AddressQuery
+    {
+        public string SearchPhrase { get; set; }
+        public int PageNumber { get; set; }
+        public int PageItems { get; set; }
+        public string SortBy { get; set; }
+        public SortDirection SortDirection { get; set; }
+
+    }
+}
---
+++ CinemaApi/Dtos/Pagination/MovieQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Dtos.Pagination
+{
+    public class MovieQuery
+    {
+        public string SearchPhrase { get; set; }
+        public int PageNumber { get; set; }
+        public int PageItems { get; set; }
+        public string SortBy { get; set; }
+        public SortDirection SortDirection { get; set; }
+    }
+}
