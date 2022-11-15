6078427b002ef7037d17bd26f032a2b19c188101
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-11-15 22:08:33 +0100

    Movie entity property add

--- CinemaApi/Entities/Movie.cs
+++ CinemaApi/Entities/Movie.cs
@@ -9,7 +9,6 @@ namespace CinemaApi.Entities
 {
     public class Movie
     {
-
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
